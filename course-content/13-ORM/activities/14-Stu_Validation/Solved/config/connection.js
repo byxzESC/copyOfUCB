9d1a25281663062c038a4322c49d393eefd3898d
Jerome Chenette <jerome.chenette@gmail.com>
2023-02-02 15:01:51 -0800

    add ORM section one solutions

--- course-content/13-ORM/activities/14-Stu_Validation/Solved/config/connection.js
+++ course-content/13-ORM/activities/14-Stu_Validation/Solved/config/connection.js
@@ -1,18 +1,14 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-// Create a connection object
 const sequelize = new Sequelize(
-  // Database name
-  'library_db',
-  // User
-  'root',
-  // Password
-  'password',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    // Database location
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    port: 3306,
   }
 );
 
