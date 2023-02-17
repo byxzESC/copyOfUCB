0c75fd05dabf0bdf9a99bc9dc28202efc0598509
Jerome Chenette <jerome.chenette@gmail.com>
2023-02-17 14:37:07 -0800

    add Computer-Science section two solutions

--- course-content/16-Computer-Science/activities/10-Stu_Callback-Queue/Solved/assets/js/script.js
+++ course-content/16-Computer-Science/activities/10-Stu_Callback-Queue/Solved/assets/js/script.js
@@ -7,20 +7,33 @@ setTimeout(() => {
   // remove the class that hides the element
   document.querySelector('form button')
     .classList.remove('display-none');
+
+    // When click() is used with supported elements (such as an <input>), 
+    // it fires the element's click event. 
+    // This event then bubbles up to elements higher in the document tree (or event chain) and fires their click events.
+    const buttEl = document.getElementById("my-button");
+    buttEl.addEventListener("click", function(e){
+      e.preventDefault();
+      console.log("I will not be submitted until clicked");
+    });
+
 }, 8000);
 
 
-const buttEl = document.getElementById("my-button");
-buttEl.addEventListener("click", function(e){
-  e.preventDefault();
-  console.log("I will not be submitted until clicked");
-});
-
+    // When click() is used with supported elements (such as an <input>), 
+    // it fires the element's click event. 
+    // This event then bubbles up to elements higher in the document tree (or event chain) and fires their click events.
+    // const buttEl = document.getElementById("my-button");
+    // buttEl.addEventListener("click", function(e){
+    //   e.preventDefault();
+    //   console.log("I will not be submitted until clicked");
+    // });
+   
 
 const formEl = document.getElementById("my-form");
 formEl.addEventListener("submit", function(e){
   e.preventDefault();
-  console.log("I can be submitted even if button is not present (hidden) - not the strtegy if you wish to prevent submission for 8 seconds");
+  console.log("I can be submitted even if button is not present - deal with that action if needed user presses enter while in input field ");
 });
 
 
